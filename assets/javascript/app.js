1b548ca3c7179ad3c81a29f0da5708d09266d22e
Weihao Rick Lee <kenchi133@hotmail.com>
2016-08-28 21:08:07 -0500

    PsudoCode

---
+++ assets/javascript/app.js
@@ -0,0 +1,31 @@
+// Click Start to start the game
+
+
+// For loop the questions until the questions are ended.
+
+// 1) Timer  (No answer: it will move on to next question)
+// 2) Question
+// 3) Provide 4 choices (mousse click)
+// 4) click the answer (provide 3 sec of "right" answer" after click or no click. and move on to next page)
+// 5) End of the game Show
+// 	Correct Answer:
+// 	Incorrect Answer:
+// 	Unanswer:
+// 	Start Over (Reset the game directly)
+
+============= 
+Title
+
+<Start>
+Time Remaining
+Question
+<List of choices>
+<Provide Respond>
+<Provide Correct Answer>
+<Provide Status>
+<Restart button>
+
+Selection of Answers
+==================
+
+
